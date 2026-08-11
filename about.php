891687d5aa3bce5539ad9be928923b95e61eb35c
QuangHuy <quanghuyhq06@gmail.com>
2026-08-11 21:14:46 +0700

    Update about.php

--- about.php
+++ about.php
@@ -46,7 +46,7 @@ $projects = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio cá nhân - <?php echo htmlspecialchars($profile['name']); ?></title>
+    <title>Hồ sơ cá nhân - <?php echo htmlspecialchars($profile['name']); ?></title>
     
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
